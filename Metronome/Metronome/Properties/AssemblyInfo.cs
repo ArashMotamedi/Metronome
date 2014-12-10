8cd8e1a2cc391a74fe3ff22fe434d32706e36a0d
ArashMotamedi <arash.motamedi@gmail.com>
2014-12-09 22:03:39 -0800

    Initial commit

---
+++ Metronome/Metronome/Properties/AssemblyInfo.cs
@@ -0,0 +1,37 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using System.Resources;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Metronome")]
+[assembly: AssemblyDescription("Play an instrument? Keep the rhythm with Metronome!")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Arash Motamedi")]
+[assembly: AssemblyProduct("Metronome")]
+[assembly: AssemblyCopyright("Copyright ©  2014")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("934ad2c1-f724-4ca7-bd77-ff4d183605cf")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Revision and Build Numbers 
+// by using the '*' as shown below:
+[assembly: AssemblyVersion("1.8.0.0")]
+[assembly: AssemblyFileVersion("1.8.0.0")]
+[assembly: NeutralResourcesLanguageAttribute("en-US")]
